2a4955c8cc2e27b6cf99f839efe24ae156b7d877
YUSRA <yusraash44@gmail.com>
2024-12-09 15:41:18 +0530

    Add files via upload

---
+++ P11/students.php
@@ -0,0 +1,14 @@
+<?php
+$students=["ADITHYA","SONALI","BASITH","CATHERIN","ZOYA","CELIN","AMEYA","DZOUZA","NICK","DEEPTHI","ARATHI"];
+print_r($students);
+echo "<br>";
+print "SORTED ARRAY:";
+echo "<br>";
+asort($students);
+print_r($students);
+echo "<br>";
+print "SORTED ARARY IN DESCENDING ORDER";
+echo "<br>";
+arsort($students);
+print_r($students);
+?>
